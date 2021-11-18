2976405bdee3998344ffae8fcbb08a000a8b4239
minaver <psmjoshua@gmail.com>
2021-11-18 16:08:15 +0900

    Add JwtServiceOwner class

---
+++ src/main/java/com/umc/hugo/util/JwtServiceOwner.java
@@ -0,0 +1,65 @@
+package com.umc.hugo.util;
+
+import com.umc.hugo.config.BaseException;
+import com.umc.hugo.config.BaseResponseStatus;
+import com.umc.hugo.config.secret.Secret;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Service
+public class JwtServiceOwner {
+
+    /*
+    JWT 생성
+    @param ownerIdx
+    @return String
+     */
+    public String createJwt(int ownerIdx){
+        Date now = new Date();
+        return Jwts.builder()
+                .setHeaderParam("type","jwt")
+                .claim("ownerIdx",ownerIdx)
+                .setIssuedAt(now)
+                .setExpiration(new Date(System.currentTimeMillis()+1*(1000*60*60*24*365)))
+                .signWith(SignatureAlgorithm.HS256, Secret.JWT_SECRET_KEY)
+                .compact();
+    }
+
+    /*
+    Header에서 X-ACCESS-TOKEN 으로 JWT 추출
+    @return String
+     */
+    public String getJwt(){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return request.getHeader("X-ACCESS-TOKEN");
+    }
+
+    public int getOwnerIdx() throws BaseException {
+        //1. JWT 추출
+        String accessToken = getJwt();
+        if(accessToken == null || accessToken.length() == 0){
+            throw new BaseException(BaseResponseStatus.EMPTY_JWT);
+        }
+
+        // 2. JWT parsing
+        Jws<Claims> claims;
+        try{
+            claims = Jwts.parser()
+                    .setSigningKey(Secret.JWT_SECRET_KEY)
+                    .parseClaimsJws(accessToken);
+        } catch (Exception ignored) {
+            throw new BaseException(BaseResponseStatus.INVALID_JWT);
+        }
+
+        // 3. ownerIdx 추출
+        return claims.getBody().get("ownerIdx",Integer.class);  // jwt 에서 ownerIdx를 추출합니다.
+    }
+}
