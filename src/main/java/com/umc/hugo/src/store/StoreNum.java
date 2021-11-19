8c070d029528fc93f1bdbc6fbdc5ce361bba6620
minaver <psmjoshua@gmail.com>
2021-11-19 13:00:37 +0900

    Add StoreNum for Paging Validation

--- src/main/java/com/umc/hugo/src/store/StoreNum.java
+++ src/main/java/com/umc/hugo/src/store/StoreNum.java
@@ -1,2 +1,14 @@
-package com.umc.hugo.src.store;public class StoreNum {
+package com.umc.hugo.src.store;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class StoreNum {
+    public StoreNum(){}
+
+    private int storeNum;
 }
